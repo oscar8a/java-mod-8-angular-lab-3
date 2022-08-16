8d4813d0c811d66108cbf73bde469662cf480da4
Oscar 8A <oscarochoacerrato@gmail.com>
2022-08-16 15:33:39 -0400

    athlete lists angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'the best simple Angular app ever!!';
+  disableNewMessage = true;
+  messages = [
+    {
+      sender: "Ludovic",
+      message: "Latest message from Ludovic"
+    },
+    {
+      sender: "Jessica",
+      message: "Latest message from Jessica"
+    }
+  ];
+  listOfAthletes = [
+    {
+      name: "Bill Russel",
+      sport: "NBA Basketball",
+      active: false
+    },
+    {
+      name: "Gabriela Sabatini",
+      sport: "WTA Tennis",
+      active: false
+    },
+    {
+      name: "Simone Biles",
+      sport: "Olympic Gymnast",
+      active: true
+    }
+  ]
+  
+  constructor() {
+    console.log("Iniating angular AppCompnent class");
+    setTimeout(() => {
+      this.disableNewMessage = !this.disableNewMessage;
+    }
+    , 2000)
+  }
+
+  onSendMessage() {
+    let message = {
+      sender: "Michael",
+      message: "New message from Michael"
+    }
+    this.messages.push(message);
+  }
 }
